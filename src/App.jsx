d2452cbc659dd49a9db62803ac473e3d63f38f3e
SadeepKhanal <sadeepkhanal343@gmail.com>
2026-02-18 18:55:49 +0545

    added form additem functionality in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,22 @@ import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+import { nanoid } from "nanoid";
+import Form from "./components/Form";
 
 const App = () => {
   const [items, setItems] = useState(groceryItems);
 
+  const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      completed: false,
+      id: nanoid(),
+    };
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    toast.success("grocery item added");
+  };
   const editCompleted = (itemId) => {
     const newItems = items.map((item) => {
       if (item.id === itemId) {
@@ -27,10 +39,12 @@ const App = () => {
   return (
     <section className="section-center">
       <ToastContainer position="top-center" />
+      <Form addItem={addItem} />
       <Items
         items={items}
         editCompleted={editCompleted}
         removeItem={removeItem}
+        
       />
     </section>
   );
